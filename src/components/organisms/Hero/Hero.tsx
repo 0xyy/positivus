94606ada7fc88779892beafd230a748abf1f97ca
Tommy Phan <tommyphan2004.tp@gmail.com>
2024-06-12 22:39:59 +0200

    🦄 refactor(Import): clean imports

--- src/components/organisms/Hero/Hero.tsx
+++ src/components/organisms/Hero/Hero.tsx
@@ -1,11 +1,9 @@
 import Typography from '~atoms/Typography/Typography';
 import { useTranslation } from '~i18n/hooks/useTranslation';
 import AppImage from '~atoms/AppImage/AppImage';
-import s from './Hero.module.scss';
 import HeroImage from '~assets/images/Hero.svg';
-
-import Image from 'next/image';
 import Button from '~atoms/Button/Button';
+import s from './Hero.module.scss';
 
 const Hero = () => {
   const { t } = useTranslation();
